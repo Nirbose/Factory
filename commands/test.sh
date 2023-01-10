e915dac6b056f886aa198f0d85e89e2a1ccbe109
Leo Tuffery <ltuffery@2A5.42angouleme.fr>
2023-01-10 20:54:09 +0100

    speed commit

--- commands/test.sh
+++ commands/test.sh
@@ -3,29 +3,41 @@ ERROR="\e[1;31m"
 BG_SUCCESS="\e[48;5;2m"
 BG_ERROR="\e[48;5;1m"
 ENDCOLOR="\e[0m"
+ELEMENT_COMPILE=""
 
-if [ "$#" -eq 0 ]; then
-    if [ $(ls tests/ | wc -l) = 0 ]; then
-        echo "There is no test"
-        exit 0
-    fi
-    echo ""
-    for file in tests/*; do
-        clang $file -o test.out
-        LINES=$(./test.out)
-        if [ $(echo $LINES | grep "Error" -c) = 0 ]; then
-            echo "${BG_SUCCESS} PASS ${ENDCOLOR} $(basename $file)"
-        else
-            echo "${BG_ERROR} ECHEC ${ENDCOLOR} $(basename $file)"
-        fi
-        for line in $LINES; do
-            if [ "$(echo $line | grep "Ok" -c)" -eq 1 ]; then
-                echo "  ${SUCCESS}✔ Ok${ENDCOLOR}"
-            else
-                echo "  ${ERROR}✖ Error${ENDCOLOR}"
-            fi
-        done
-        echo ""
-    done
-    rm test.out
+if ! [ $# = 0 ]; then
+	if [ $1 = "-w" ]; then
+		if ! [ $# = 2 ]; then
+			echo "Missing libs/file to add at compile time"
+			exit 1
+		fi
+		ELEMENT_COMPILE=$(dirname $0)/$2
+	fi
 fi
+
+if [ $(ls tests/ | wc -l) = 0 ]; then
+	echo "There is no test"
+	exit 0
+fi
+echo ""
+for file in tests/*; do
+	if ! [ -f $file ]; then
+		continue
+	fi
+	clang $file $ELEMENT_COMPILE -o test.out
+	LINES=$(./test.out)
+	if [ $(echo $LINES | grep "Error" -c) = 0 ]; then
+		echo "${BG_SUCCESS} PASS ${ENDCOLOR} $(basename $file)"
+	else
+		echo "${BG_ERROR} ECHEC ${ENDCOLOR} $(basename $file)"
+	fi
+	for line in $LINES; do
+		if [ "$(echo $line | grep "Ok" -c)" -eq 1 ]; then
+			echo "  ${SUCCESS}✔ Ok${ENDCOLOR}"
+		else
+			echo "  ${ERROR}✖ Error${ENDCOLOR}"
+		fi
+	done
+	echo ""
+done
+rm test.out
