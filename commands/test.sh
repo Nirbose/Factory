11b121fffb90660fa1f4e9d224bc25381a29e3af
Leo Tuffery <ltuffery@2C2.42angouleme.fr>
2023-01-11 10:04:02 +0100

    update test add with feat

--- commands/test.sh
+++ commands/test.sh
@@ -5,39 +5,50 @@ BG_ERROR="\e[48;5;1m"
 ENDCOLOR="\e[0m"
 ELEMENT_COMPILE=""
 
-if ! [ $# = 0 ]; then
-	if [ $1 = "-w" ]; then
-		if ! [ $# = 2 ]; then
-			echo "Missing libs/file to add at compile time"
-			exit 1
-		fi
-		ELEMENT_COMPILE=$(dirname $0)/$2
+run_test() {
+	if [ $(ls tests/ | wc -l) = 0 ]; then
+		echo "There is no test"
+		exit 0
 	fi
-fi
+	echo ""
+	for file in tests/*.c; do
+		if ! [ -f $file ]; then
+			continue
+		fi
+		clang $file $ELEMENT_COMPILE -o test.out
+		if ! [ $? = 0 ]; then
+			echo "${BG_ERROR} ERROR ${ENDCOLOR} Compilation error!"
+			continue
+		fi
+		LINES=$(./test.out)
+		if [ $(echo $LINES | grep "Error" -c) = 0 ]; then
+			echo "${BG_SUCCESS} PASS ${ENDCOLOR} $(basename $file)"
+		else
+			echo "${BG_ERROR} ECHEC ${ENDCOLOR} $(basename $file)"
+		fi
+		for line in $LINES; do
+			if [ "$(echo $line | grep "Ok" -c)" -eq 1 ]; then
+				echo "  ${SUCCESS}✔ Ok${ENDCOLOR}"
+			else
+				echo "  ${ERROR}✖ Error${ENDCOLOR}"
+			fi
+		done
+		echo ""
+	done
+	rm test.out
+}
 
-if [ $(ls tests/ | wc -l) = 0 ]; then
-	echo "There is no test"
+if [ $# = 0 ]; then
+	run_test
 	exit 0
 fi
-echo ""
-for file in tests/*; do
-	if ! [ -f $file ]; then
-		continue
+
+if [ $1 = "with" ]; then
+	if [ $# = 1 ]; then
+		echo "The files with which you wanted to compile are missing"
+		exit 0
 	fi
-	clang $file $ELEMENT_COMPILE -o test.out
-	LINES=$(./test.out)
-	if [ $(echo $LINES | grep "Error" -c) = 0 ]; then
-		echo "${BG_SUCCESS} PASS ${ENDCOLOR} $(basename $file)"
-	else
-		echo "${BG_ERROR} ECHEC ${ENDCOLOR} $(basename $file)"
-	fi
-	for line in $LINES; do
-		if [ "$(echo $line | grep "Ok" -c)" -eq 1 ]; then
-			echo "  ${SUCCESS}✔ Ok${ENDCOLOR}"
-		else
-			echo "  ${ERROR}✖ Error${ENDCOLOR}"
-		fi
-	done
-	echo ""
-done
-rm test.out
+	ELEMENT_COMPILE=$2
+	run_test
+	exit 0
+fi
